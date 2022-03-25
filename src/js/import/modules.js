06adbb853aef6669bfcbe4f15eae0fcaf240cceb
amer1can <amer1can@mail.ru>
2022-03-25 15:01:29 +0300

    sign-in create-acc done

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,4 +2,5 @@ import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/index/index";
 import "%modules%/sign-in/sign-in";
+import "%modules%/create-acc/create-acc";
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,43 @@
 import "./import/modules";
 import "./import/components";
+
+
+// SIGN-IN
+const signIn = document.querySelector('#sign-in')
+const signInBtn = document.querySelector('.header__sign-btn')
+const closeSignInBtn = signIn.querySelector('.sign-in__close')
+
+signInBtn.addEventListener('click', ()=> {
+    signIn.classList.add('visible')
+    document.body.classList.add('sign')
+})
+
+closeSignInBtn.addEventListener('click', ()=> {
+    signIn.classList.remove('visible')
+    document.body.classList.remove('sign')
+})
+
+// CREATE ACCOUNT
+const createAcc = document.querySelector('#create-acc')
+const createAccBtn = document.querySelector('.sign-in__create-link')
+const closeCreateAccBtn = createAcc.querySelector('.create-acc__close')
+const goBack = createAcc.querySelector('.create-acc__back')
+
+createAccBtn.addEventListener('click', ()=> {
+    signIn.classList.remove('visible')
+    document.body.classList.remove('sign')
+    createAcc.classList.add('visible')
+    document.body.classList.add('create')
+})
+
+closeCreateAccBtn.addEventListener('click', ()=> {
+    createAcc.classList.remove('visible')
+    document.body.classList.remove('create')
+})
+
+goBack.addEventListener('click', ()=> {
+    createAcc.classList.remove('visible')
+    document.body.classList.remove('create')
+    signIn.classList.add('visible')
+    document.body.classList.add('sign')
+})
